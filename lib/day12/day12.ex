49366acbdd776adf4edab626a759adc910fea5ee
Eric Vinson <esvinson@gmail.com>
2019-12-16 06:23:41 -0700

    Day 12 Part 2

--- lib/day12/day12.ex
+++ lib/day12/day12.ex
@@ -97,8 +97,79 @@ defmodule Advent.Day12 do
     |> calculate_energy()
   end
 
+  def matches?(i, n) when i == n, do: true
+  def matches?(_, _), do: false
+
+  def get_x({{x, _, _}, {vx, _, _}}), do: {x, vx}
+  def get_y({{_, y, _}, {_, vy, _}}), do: {y, vy}
+  def get_z({{_, _, z}, {_, _, vz}}), do: {z, vz}
+
+  def test1_part2 do
+    """
+    <x=-1, y=0, z=2>
+    <x=2, y=-10, z=-7>
+    <x=4, y=-8, z=8>
+    <x=3, y=5, z=-1>
+    """
+    |> parse_input()
+    |> solve2()
+  end
+
+  def test2_part2 do
+    """
+    <x=-8, y=-10, z=0>
+    <x=5, y=5, z=10>
+    <x=2, y=-7, z=3>
+    <x=9, y=-8, z=-3>
+    """
+    |> parse_input()
+    |> solve2()
+  end
+
+  def solve2(initial_state) do
+    init_x = Enum.map(initial_state, &get_x/1)
+    init_y = Enum.map(initial_state, &get_y/1)
+    init_z = Enum.map(initial_state, &get_z/1)
+
+    Enum.reduce_while(
+      Stream.iterate(1, &(&1 + 1)),
+      {initial_state, initial_state, {nil, nil, nil}},
+      fn x, {initial_state, current_state, {ax, ay, az} = answer} ->
+        new_state =
+          current_state
+          |> adjust_velocities()
+          |> apply_velocities()
+
+        new_x = Enum.map(new_state, &get_x/1)
+        new_y = Enum.map(new_state, &get_y/1)
+        new_z = Enum.map(new_state, &get_z/1)
+
+        new_answer = if is_nil(ax) && matches?(init_x, new_x), do: {x, ay, az}, else: answer
+        new_answer = if is_nil(ay) && matches?(init_y, new_y), do: {ax, x, az}, else: new_answer
+        new_answer = if is_nil(az) && matches?(init_z, new_z), do: {ax, ay, x}, else: new_answer
+
+        new_answer
+        |> case do
+          {ax, ay, az} when not is_nil(ax) and not is_nil(ay) and not is_nil(az) ->
+            {:halt, new_answer}
+
+          _ ->
+            {:cont, {initial_state, new_state, new_answer}}
+        end
+      end
+    )
+    |> lcm()
+  end
+
+  defp gcd(a, 0), do: abs(a)
+  defp gcd(a, b), do: gcd(b, rem(a, b))
+
+  defp lcm(a, b), do: div(abs(a * b), gcd(a, b))
+  defp lcm({x, y, z}), do: lcm(x, lcm(y, z))
+
   def part2 do
     Advent.daily_input(12)
     |> parse_input()
+    |> solve2()
   end
 end
