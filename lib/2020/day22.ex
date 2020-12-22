f91e22fac585ee229266ee481fe4c9bc0bf8716e
Eric Vinson <esvinson@gmail.com>
2020-12-22 10:03:34 -0700

    Day 22 - Part 2

--- lib/2020/day22.ex
+++ lib/2020/day22.ex
@@ -54,6 +54,57 @@ defmodule Aoc202022 do
     |> calculate_score()
   end
 
+  def play_recursive({p1_cards, []}, _history), do: {:player1, p1_cards}
+  def play_recursive({[], p2_cards}, _history), do: {:player2, p2_cards}
+
+  def play_recursive(
+        {[p1_card | p1_remaining] = p1_cards, [p2_card | p2_remaining] = p2_cards},
+        history
+      ) do
+    card_set = {List.to_tuple(p1_cards), List.to_tuple(p2_cards)}
+
+    Enum.member?(history, card_set)
+    |> case do
+      true ->
+        {:player1, p1_cards}
+
+      false ->
+        if Enum.count(p1_remaining) >= p1_card && Enum.count(p2_remaining) >= p2_card do
+          play_recursive(
+            {p1_remaining |> Enum.take(p1_card), p2_remaining |> Enum.take(p2_card)},
+            []
+          )
+          |> case do
+            {:player1, _} ->
+              play_recursive({p1_remaining ++ [p1_card, p2_card], p2_remaining}, [
+                card_set | history
+              ])
+
+            {:player2, _} ->
+              play_recursive({p1_remaining, p2_remaining ++ [p2_card, p1_card]}, [
+                card_set | history
+              ])
+          end
+        else
+          if p1_card > p2_card do
+            play_recursive({p1_remaining ++ [p1_card, p2_card], p2_remaining}, [
+              card_set | history
+            ])
+          else
+            play_recursive({p1_remaining, p2_remaining ++ [p2_card, p1_card]}, [
+              card_set | history
+            ])
+          end
+        end
+    end
+  end
+
+  def part2(card_sets) do
+    card_sets
+    |> play_recursive([])
+    |> calculate_score()
+  end
+
   def run do
     test_input1 =
       test_input()
@@ -62,8 +113,8 @@ defmodule Aoc202022 do
     test_result1 = part1(test_input1)
     IO.puts("Solution to Test Part 1 (Should be 306): #{inspect(test_result1)}")
 
-    # test_result2 = part2(test_input1)
-    # IO.puts("Solution to Test Part 2 (Should be 273): #{inspect(test_result2)}")
+    test_result2 = part2(test_input1)
+    IO.puts("Solution to Test Part 2 (Should be 291): #{inspect(test_result2)}")
 
     input =
       Advent.daily_input("2020", "22")
@@ -72,7 +123,7 @@ defmodule Aoc202022 do
     result1 = part1(input)
     IO.puts("Solution to Part 1: #{result1}")
 
-    # result2 = part2(input)
-    # IO.puts("Solution to Part 2: #{result2}")
+    result2 = part2(input)
+    IO.puts("Solution to Part 2: #{result2}")
   end
 end
