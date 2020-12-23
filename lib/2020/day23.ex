48ca802825a07e4d377be7187e79b3eeb7b1f513
Eric Vinson <esvinson@gmail.com>
2020-12-23 15:38:22 -0700

    Day 23 - Part 2 + refactor to not use lists

--- lib/2020/day23.ex
+++ lib/2020/day23.ex
@@ -17,28 +17,69 @@ defmodule Aoc202023 do
     end)
   end
 
-  def play(cups, _max, 0), do: cups
+  def output_part1(cups, id \\ 1) do
+    cups
+    |> Map.get(id)
+    |> case do
+      1 -> []
+      cup_id -> [cup_id | output_part1(cups, cup_id)]
+    end
+  end
 
-  def play([current, first, second, third | rest], max, rounds_left) do
+  def output_part2(cups) do
+    first = Map.get(cups, 1)
+    second = Map.get(cups, first)
+    first * second
+  end
+
+  def play(cups, _current, _max, 0), do: cups
+
+  def play(cups, current, max, rounds_left) do
+    first = Map.get(cups, current)
+    second = Map.get(cups, first)
+    third = Map.get(cups, second)
+    next = Map.get(cups, third)
+
     destination = determine_destination([first, second, third], current - 1, max)
-    new_rest = rebuild_rest(rest, destination, [first, second, third])
+    destination_next = Map.get(cups, destination)
 
-    play(
-      new_rest ++ [current],
+    cups
+    |> Map.put(current, next)
+    |> Map.put(destination, first)
+    |> Map.put(third, destination_next)
+    |> play(
+      next,
       max,
       rounds_left - 1
     )
   end
 
-  def part1(input, rounds) do
+  def build_map(list, output \\ %{}, first \\ nil)
+  def build_map([current | _rest] = list, output, nil), do: build_map(list, output, current)
+  # One element left
+  def build_map([current], output, first) do
+    Map.put(output, current, first)
+  end
+
+  def build_map([current, next | rest], output, first) do
+    build_map([next | rest], Map.put(output, current, next), first)
+  end
+
+  def part1([first | _rest] = input, rounds) do
     max = Enum.max(input)
+    cups = build_map(input)
 
-    [left, right] =
-      play(input, max, rounds)
-      |> Enum.join("")
-      |> String.split("1")
+    play(cups, first, max, rounds)
+    |> output_part1(1)
+    |> Enum.join()
+  end
 
-    "#{right}#{left}"
+  def part2([first | _rest] = input) do
+    max = Enum.max(input)
+    cups = build_map(input ++ Enum.to_list((max + 1)..1_000_000))
+
+    play(cups, first, 1_000_000, 10_000_000)
+    |> output_part2()
   end
 
   def run do
@@ -51,7 +92,7 @@ defmodule Aoc202023 do
     result1 = part1(input, 100)
     IO.puts("Solution to Part 1: #{result1}")
 
-    # result1 = part2(input)
-    # IO.puts("Solution to Part 2: #{result1}")
+    result2 = part2(input)
+    IO.puts("Solution to Part 2: #{result2}")
   end
 end
