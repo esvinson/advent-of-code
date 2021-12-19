879dbd5dedb4163aff14990c06c5022503d36bff
Eric Vinson <esvinson@gmail.com>
2021-12-18 19:53:43 -0700

    2021 - Day 8 - Part 2

--- lib/2021/day8.ex
+++ lib/2021/day8.ex
@@ -18,8 +18,71 @@ defmodule Aoc202108 do
     |> Enum.sum()
   end
 
+  defp solve_row(input, output) do
+    [one] = Enum.filter(input, &(length(&1) == 2))
+    [seven] = Enum.filter(input, &(length(&1) == 3))
+    [four] = Enum.filter(input, &(length(&1) == 4))
+    [eight] = Enum.filter(input, &(length(&1) == 7))
+    [two] = Enum.filter(input, &(length(&1) == 5 and length(four -- &1) == 2))
+    [three] = Enum.filter(input, &(length(&1) == 5 and length(one -- &1) == 0))
+    [six] = Enum.filter(input, &(length(&1) == 6 and length(one -- &1) == 1))
+    [nine] = Enum.filter(input, &(length(&1) == 6 and length(four -- &1) == 0))
+
+    [zero] =
+      Enum.filter(
+        input,
+        &(length(&1) == 6 and length(four -- &1) == 1 and length(one -- &1) == 0)
+      )
+
+    [five] =
+      Enum.filter(
+        input,
+        &(length(&1) == 5 and length(four -- &1) == 1 and length(one -- &1) == 1)
+      )
+
+    mapping = [zero, one, two, three, four, five, six, seven, eight, nine]
+
+    output
+    |> Enum.map(fn set ->
+      Enum.find_index(mapping, &(&1 == set))
+    end)
+    |> Integer.undigits()
+  end
+
   def part2(initial) do
-    initial
+    Enum.map(initial, fn [input, output] ->
+      input =
+        input
+        |> String.split(" ", trim: true)
+        |> Enum.map(fn signals ->
+          signals
+          |> String.split("", trim: true)
+          |> Enum.sort()
+        end)
+
+      output =
+        output
+        |> String.split(" ", trim: true)
+        |> Enum.map(fn signals ->
+          signals
+          |> String.split("", trim: true)
+          |> Enum.sort()
+        end)
+
+      solve_row(input |> Enum.sort_by(&length/1), output)
+    end)
+    |> Enum.sum()
+
+    # 0 = length 6, three segments from 4, and all from 1
+    # 1 = length 2
+    # 2 = length 5, two segments from 4
+    # 3 = length 5, all segments from 1
+    # 4 = length 4
+    # 5 = length 5, three segments from 4
+    # 6 = length 6, one segment from 1
+    # 7 = length 3
+    # 8 = length 7
+    # 9 = length 6, all segments from 4
   end
 
   def run do
@@ -33,7 +96,7 @@ defmodule Aoc202108 do
 
     IO.puts("Test Answer Part 1: #{inspect(part1(test_input))}")
     IO.puts("Part 1: #{inspect(part1(input))}")
-    # IO.puts("Test Answer Part 2: #{inspect(part2(test_input))}")
-    # IO.puts("Part 2: #{inspect(part2(input))}")
+    IO.puts("Test Answer Part 2: #{inspect(part2(test_input))}")
+    IO.puts("Part 2: #{inspect(part2(input))}")
   end
 end
