0707d0c5ac4a29ad85932f3f788259c58f4d8210
Eric Vinson <esvinson@gmail.com>
2022-12-04 10:45:58 -0700

    2022 - Day 4 Part 1

---
+++ lib/2022/day4.ex
@@ -0,0 +1,52 @@
+defmodule Aoc202204 do
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+  end
+
+  def part1(assignments) do
+    assignments
+    |> Enum.reduce(0, fn set, acc ->
+      [left, right] =
+        set
+        |> String.split(",", trim: true)
+
+      [start, stop] = String.split(left, "-", trim: true) |> Enum.map(&String.to_integer/1)
+      first = MapSet.new(Range.new(start, stop))
+      [start, stop] = String.split(right, "-", trim: true) |> Enum.map(&String.to_integer/1)
+      second = MapSet.new(Range.new(start, stop))
+
+      if MapSet.subset?(first, second) or MapSet.subset?(second, first) do
+        acc + 1
+      else
+        acc
+      end
+    end)
+  end
+
+  def part2(assignments) do
+    assignments
+  end
+
+  def run do
+    test_input =
+      """
+      2-4,6-8
+      2-3,4-5
+      5-7,7-9
+      2-8,3-7
+      6-6,4-6
+      2-6,4-8
+      """
+      |> parse
+
+    input =
+      Advent.daily_input("2022", "04")
+      |> parse()
+
+    IO.puts("Test Answer Part 1: #{inspect(part1(test_input))}")
+    IO.puts("Part 1: #{inspect(part1(input))}")
+    # IO.puts("Test Answer Part 2: #{inspect(part2(test_input))}")
+    # IO.puts("Part 2: #{inspect(part2(input))}")
+  end
+end
