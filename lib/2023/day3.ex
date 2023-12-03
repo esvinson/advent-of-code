9b750fad607c506cc02518f375abd2ad0038c34d
Eric Vinson <esvinson@gmail.com>
2023-12-03 15:54:14 -0700

    Day 3 Refactor

--- lib/2023/day3.ex
+++ lib/2023/day3.ex
@@ -13,87 +13,6 @@ defmodule Aoc202303 do
     end)
   end
 
-  defp point_sort({_x, y}, {_x2, y2}) when y > y2, do: false
-  defp point_sort({_x, y}, {_x2, y2}) when y < y2, do: true
-  defp point_sort({x, _y}, {x2, _y2}) when x > x2, do: false
-  defp point_sort({x, _y}, {x2, _y2}) when x < x2, do: true
-  defp point_sort(_, _), do: true
-
-  defp adjacent_symbols(map, {x, y}) do
-    for(
-      i <- -1..1,
-      j <- -1..1,
-      x + i >= 0 and y + j >= 0 and {i, j} not in [{0, 0}] and Map.has_key?(map, {x + i, y + j}) and
-        Regex.match?(~r/^[^\d]$/, Map.get(map, {x + i, y + j})),
-      do: {x + i, y + j}
-    )
-  end
-
-  # defp build_number(map, {x, y}) do
-  #   # if Regex.match?(~r/^\d$/, Map.get(map, {x + 1, y})) do
-  #   # Map.get()
-  #   ""
-  # end
-
-  defp digit_to_left?(map, {x, y}), do: Regex.match?(~r/\d/, Map.get(map, {x - 1, y}, ""))
-  defp digit_to_right?(map, {x, y}), do: Regex.match?(~r/\d/, Map.get(map, {x + 1, y}, ""))
-
-  defp traverse([], _, _, output), do: output
-
-  defp traverse([current | rest], map, has_symbol?, output) do
-    if digit_to_left?(map, current) do
-      # We will have handled all these from the first number in the string, skip
-      traverse(rest, map, has_symbol?, output)
-    else
-      init = {current, Map.get(map, current), Map.get(has_symbol?, current)}
-
-      {_, out_string, symbol?} =
-        Enum.reduce_while(0..5, init, fn _, {{x2, y2} = pos, num_string, symbol?} ->
-          if digit_to_right?(map, pos) do
-            {:cont,
-             {{x2 + 1, y2}, num_string <> Map.get(map, {x2 + 1, y2}),
-              symbol? or Map.get(has_symbol?, {x2 + 1, y2})}}
-          else
-            {:halt, {pos, num_string, symbol?}}
-          end
-        end)
-
-      traverse(
-        rest,
-        map,
-        has_symbol?,
-        [%{value: String.to_integer(out_string), symbol?: symbol?}] ++ output
-      )
-    end
-  end
-
-  defp build_numbers(map) do
-    has_symbol? =
-      map
-      |> Map.keys()
-      |> Enum.sort(&point_sort/2)
-      |> Enum.reduce(%{}, fn pos, acc ->
-        if Regex.match?(~r/^\d$/, Map.get(map, pos)) do
-          Map.put(acc, pos, Enum.count(adjacent_symbols(map, pos)) > 0)
-        else
-          acc
-        end
-      end)
-
-    has_symbol?
-    |> Map.keys()
-    |> Enum.sort(&point_sort/2)
-    |> traverse(map, has_symbol?, [])
-  end
-
-  defp part1(mapped_input) do
-    mapped_input
-    |> build_numbers()
-    |> Enum.filter(fn %{symbol?: symbol?} -> symbol? end)
-    |> Enum.map(& &1[:value])
-    |> Enum.sum()
-  end
-
   defp adjacent_numbers(map, {x, y}) do
     for(
       i <- -1..1,
@@ -114,34 +33,60 @@ defmodule Aoc202303 do
       else: Map.get(map, pos)
   end
 
-  defp gear_ratios(map) do
+  defp find_adjacent(map, filter) do
     map
     |> Map.keys()
     |> Enum.sort(&point_sort/2)
-    |> Enum.reduce(0, fn pos, acc ->
-      if Regex.match?(~r/^\*$/, Map.get(map, pos)) do
+    |> Enum.reduce([], fn pos, acc ->
+      if Regex.match?(filter, Map.get(map, pos)) do
         unique_adjacent =
           adjacent_numbers(map, pos)
           |> Enum.map(&parent_position(map, &1))
           |> MapSet.new()
           |> MapSet.to_list()
 
-        if Enum.count(unique_adjacent) > 1 do
-          [i, j] = Enum.map(unique_adjacent, &String.to_integer(point_to_number(map, &1)))
-
-          acc + i * j
-        else
-          acc
-        end
+        [{pos, unique_adjacent}] ++ acc
       else
         acc
       end
     end)
+    |> Map.new()
   end
 
-  defp part2(mapped_input) do
-    mapped_input
-    |> gear_ratios()
+  defp point_sort({_x, y}, {_x2, y2}) when y > y2, do: false
+  defp point_sort({_x, y}, {_x2, y2}) when y < y2, do: true
+  defp point_sort({x, _y}, {x2, _y2}) when x > x2, do: false
+  defp point_sort({x, _y}, {x2, _y2}) when x < x2, do: true
+  defp point_sort(_, _), do: true
+
+  defp digit_to_left?(map, {x, y}), do: Regex.match?(~r/\d/, Map.get(map, {x - 1, y}, ""))
+  defp digit_to_right?(map, {x, y}), do: Regex.match?(~r/\d/, Map.get(map, {x + 1, y}, ""))
+
+  defp part1(map) do
+    map
+    |> find_adjacent(~r/^[^\d]$/)
+    |> Enum.reduce(0, fn {_, unique_adjacent}, acc ->
+      sum =
+        unique_adjacent
+        |> Enum.map(&String.to_integer(point_to_number(map, &1)))
+        |> Enum.sum()
+
+      acc + sum
+    end)
+  end
+
+  defp part2(map) do
+    map
+    |> find_adjacent(~r/^\*$/)
+    |> Enum.reduce(0, fn {_, unique_adjacent}, acc ->
+      if Enum.count(unique_adjacent) > 1 do
+        [i, j] = Enum.map(unique_adjacent, &String.to_integer(point_to_number(map, &1)))
+
+        acc + i * j
+      else
+        acc
+      end
+    end)
   end
 
   def run() do
