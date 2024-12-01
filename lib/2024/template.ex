a2e7d0ca923f8398efb6c51b0a324327bbd6989c
Eric Vinson <esvinson@gmail.com>
2024-12-01 08:50:33 -0700

    2024 template

---
+++ lib/2024/template.ex
@@ -0,0 +1,30 @@
+# defmodule Aoc2024## do
+#   defp parse(input) do
+#     input
+#     |> String.split("\n", trim: true)
+#   end
+
+#   defp part1(input) do
+#     input
+#   end
+
+#   # defp part2(input) do
+#   #   input
+#   # end
+
+#   def run() do
+#     test_input =
+#       """
+#       """
+#       |> parse()
+
+#     # input =
+#     #   Advent.daily_input("2024", "##")
+#     #   |> parse()
+
+#     IO.puts("Test Answer Part 1: #{inspect(part1(test_input))}")
+#     # IO.puts("Part 1: #{inspect(part1(input))}")
+#     # IO.puts("Test Answer Part 2: #{inspect(part2(test_input))}")
+#     # IO.puts("Part 2: #{inspect(part2(input))}")
+#   end
+# end
