55e70219a9cbafc90686c902c1008625c3834940
Eric Vinson <esvinson@gmail.com>
2025-12-02 22:58:09 -0700

    Day 3 - Part 1 - Attempt 2

--- lib/2025/day3.ex
+++ lib/2025/day3.ex
@@ -11,17 +11,28 @@ defmodule Aoc202503 do
     if a > max1 do
       find_biggest(rest, a, max1)
     else
-      find_biggest(rest, max1, max2)
+      if Enum.find(rest, false, fn x -> x == max1 end) do
+        if a > max2 do
+          find_biggest(rest, max1, a)
+        else
+          find_biggest(rest, max1, max2)
+        end
+      else
+        find_biggest(rest, max1, max2)
+      end
     end
   end
 
   defp part1(banks) do
     Enum.map(banks, fn bank ->
+      # IO.inspect(bank, label: "bank ===>", printable_limit: :infinity, limit: :infinity)
       rbank = Enum.reverse(bank)
       [first | rest] = rbank
       [a, b] = find_biggest(rbank, 0, 0)
       [a, b] = if b == 0, do: [List.first(find_biggest(rest, 0, 0)), first], else: [a, b]
+
       Integer.undigits([a, b])
+      # |> IO.inspect(label: "RESULT ===>")
     end)
     |> Enum.sum()
   end
@@ -44,10 +55,9 @@ defmodule Aoc202503 do
       Advent.daily_input("2025", "03")
       |> parse()
 
-    IO.puts("Test Answer Part 1: #{inspect(part1(test_input), charlists: :as_lists)}")
-    # Not 17073
+    IO.puts("Test Answer Part 1 (357): #{inspect(part1(test_input), charlists: :as_lists)}")
+    # Not 17073, Not 17208
     IO.puts("Part 1: #{inspect(part1(input), charlists: :as_lists)}")
-
     # IO.puts("Test Answer Part 2: #{inspect(part2(test_input), charlists: :as_lists)}")
     # IO.puts("Part 2: #{inspect(part2(input), charlists: :as_lists)}")
   end
