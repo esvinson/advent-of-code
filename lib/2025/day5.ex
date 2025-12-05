e82cb69845b50f8e9d31ee6dbff53b4588eb87f2
Eric Vinson <esvinson@gmail.com>
2025-12-04 22:38:32 -0700

    2025 - Day 5 - Part 2

--- lib/2025/day5.ex
+++ lib/2025/day5.ex
@@ -13,7 +13,7 @@ defmodule Aoc202505 do
           |> String.split("-", trim: true)
           |> Enum.map(&String.to_integer/1)
 
-        Range.new(start, stop)
+        {start, stop}
       end)
 
     ingredients =
@@ -23,14 +23,37 @@ defmodule Aoc202505 do
   end
 
   defp part1({fresh, ingredients}) do
+    fresh = Enum.map(fresh, fn {start, stop} -> Range.new(start, stop) end)
+
     Enum.reduce(ingredients, 0, fn ingredient, acc ->
       if Enum.any?(fresh, fn range -> ingredient in range end), do: acc + 1, else: acc
     end)
   end
 
-  # defp part2(input) do
-  #   input
-  # end
+  defp part2({fresh, _}) do
+    {last, rest} =
+      fresh
+      |> Enum.sort()
+      |> Enum.reduce({{0, 0}, []}, fn {start, stop}, {{pstart, pstop}, acc} ->
+        if start >= pstart and start <= pstop and stop > pstop do
+          {{pstart, stop}, acc}
+        else
+          if start >= pstart and start <= pstop and stop <= pstop do
+            {{pstart, pstop}, acc}
+          else
+            {{start, stop}, [{pstart, pstop}] ++ acc}
+          end
+        end
+      end)
+
+    ([last] ++ rest)
+    |> Enum.filter(fn
+      {0, 0} -> false
+      _ -> true
+    end)
+    |> Enum.map(fn {start, stop} -> stop - start + 1 end)
+    |> Enum.sum()
+  end
 
   def run() do
     test_input =
@@ -55,7 +78,8 @@ defmodule Aoc202505 do
 
     IO.puts("Test Answer Part 1: #{inspect(part1(test_input), charlists: :as_lists)}")
     IO.puts("Part 1: #{inspect(part1(input), charlists: :as_lists)}")
-    # IO.puts("Test Answer Part 2: #{inspect(part2(test_input), charlists: :as_lists)}")
-    # IO.puts("Part 2: #{inspect(part2(input), charlists: :as_lists)}")
+    IO.puts("Test Answer Part 2: #{inspect(part2(test_input), charlists: :as_lists)}")
+    # Not 378661413228203
+    IO.puts("Part 2: #{inspect(part2(input), charlists: :as_lists)}")
   end
 end
